30dca2c5b047bdf3eff9bc37f1f94e6365acea2f
yoshiori <yoshiori@gmail.com>
2013-11-28 19:34:29 +0900

    Add scan feature.

--- lib/baby_face.rb
+++ lib/baby_face.rb
@@ -1,5 +1,43 @@
 require "baby_face/version"
 
 module BabyFace
-  # Your code goes here...
+
+  def BabyFace.included(mod)
+    mod.define_singleton_method(
+      :acts_as_feature,
+      lambda { |*args|
+        mod.class_variable_set(:@@_features, args)
+      }
+    )
+  end
+
+  def to_feature
+    def scan(prefix, obj)
+      obj.class.class_variable_get(:@@_features).map do |attr|
+        _prefix = prefix.nil? ? attr : "#{prefix}_#{attr}"
+        value = obj.send(attr)
+        case value
+        when BabyFace
+          scan(_prefix, value)
+        when Array
+          value.map do |val|
+            scan(_prefix, val)
+          end
+        when Hash
+          value.map do |key, val|
+            scan("#{_prefix}_#{key}", val)
+          end
+        else
+          wakachi(value.to_s).map do |text|
+            "#{_prefix}_#{text}"
+          end
+        end
+      end
+    end
+    scan(nil, self).flatten.join(" ")
+  end
+
+  def wakachi(text)
+    text.split
+  end
 end
--- spec/baby_face_spec.rb
+++ spec/baby_face_spec.rb
@@ -1,10 +1,66 @@
 require 'baby_face'
 
 describe BabyFace do
-  class Hoge
-    include BabyFace
+
+  describe "#to_feature" do
+    subject { target.to_feature }
+
+    class Hoge
+      include BabyFace
+      attr_accessor :title, :name
+      acts_as_feature :title, :name
+
+      def initialize(title: "Revenge of the Sith", name: "Anakin Skywalker")
+        @title = title
+        @name = name
+      end
+    end
+
+    class Bar
+      include BabyFace
+      attr_accessor :attr1, :attr2
+      acts_as_feature :attr1, :attr2
+
+      def initialize(attr1: "foo", attr2: "bar")
+        @attr1 = attr1
+        @attr2 = attr2
+      end
+    end
+
+    context "simple object" do
+      let(:target) { Hoge.new }
+
+      it { should eq "title_Revenge title_of title_the title_Sith name_Anakin name_Skywalker" }
+    end
+
+    context "nested object" do
+      let(:target) { Hoge.new(name: Bar.new) }
+
+      it { should eq "title_Revenge title_of title_the title_Sith name_attr1_foo name_attr2_bar" }
+    end
+
+    context "nested array" do
+      let(:target) { Hoge.new(name: [Bar.new, Bar.new]) }
+
+      it { should eq "title_Revenge title_of title_the title_Sith name_attr1_foo name_attr2_bar name_attr1_foo name_attr2_bar" }
+    end
+
+    context "nested hash" do
+      let(:target) { Hoge.new(name: {bar1: Bar.new,bar2: Bar.new}) }
+
+      it { should eq "title_Revenge title_of title_the title_Sith name_bar1_attr1_foo name_bar1_attr2_bar name_bar2_attr1_foo name_bar2_attr2_bar" }
+    end
   end
 
+  describe "#wakachi" do
+    context "default" do
+      class Dummy
+        include BabyFace
+      end
 
-  it { Hoge.new.should_not be_nil }
+      let(:baby_face) { Dummy.new }
+
+      it { expect(baby_face.wakachi("aaa bbb")).to eq ["aaa", "bbb"] }
+    end
+  end
 end
